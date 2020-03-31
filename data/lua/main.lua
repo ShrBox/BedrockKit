763bfece2c1ccce717c3edbfa3a8e16065afc65e
Sysca11 <sysca11@github.com>
2020-03-31 10:47:25 +0800

    data files,documention

---
+++ data/lua/main.lua
@@ -0,0 +1,15 @@
+function u_kksk(name)
+	sendText(name,"123123")
+	print("money",lbind("getMoney","steve"))
+-- print(lbind("addMoney","steve",114514,"kksk"))
+	oList_=oList()
+	for k,v in pairs(oList_) do
+		print(k,v)
+	end
+end
+function CB(name,dat1,dat2)
+	print(dat1[2],dat2[1])
+end
+function u_k(name)
+	GUI(name,"a","text1","text2","text3")
+end
